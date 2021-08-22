16f8b2fb7b7bf21e7aff28a57a80a9292c1579d7
Jacob <Jacobmarq@gmail.com>
2021-08-22 07:47:24 -0700

    implement post creation

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,4 @@
 class Article < ApplicationRecord
     validates :title, presence: true
-    validates :body, presence: true, length: { minimum: 10 }
+    validates :body, presence: true, length: { minimum: 10, too_short: "must contain atleast %{count} characters", maximum: 500 , too_long: "may not exceed %{count} characters"}
 end
