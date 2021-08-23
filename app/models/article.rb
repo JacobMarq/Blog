1108391bf05f27f706db7fde3ff6d64727224c85
Jacob <Jacobmarq@gmail.com>
2021-08-23 05:22:05 -0700

    implement article comments

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,6 @@
 class Article < ApplicationRecord
+    has_many :comments
+
     validates :title, presence: true
     validates :body, presence: true, length: { minimum: 10, too_short: "must contain atleast %{count} characters", maximum: 500 , too_long: "may not exceed %{count} characters"}
 end
