6ac965d30bc92a8586d7d1b9680bb612ada5f396
Jacobmarq <jacobmarq@gmail.com>
2022-12-06 06:38:11 -0800

    edit styling for images articles and users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,11 +20,13 @@ class UsersController < ApplicationController
     def follow
         @user = User.find(params[:id])
         Friendship.create(follower_id: current_user.id, follows_id: @user.id)
+        redirect_back(fallback_location: @user)
     end
 
     def unfollow
         @user = User.find(params[:id])
         @friendship = @user.select_friendship(current_user)
         @friendship.destroy
+        redirect_back(fallback_location: @user)
     end
 end
